3bfe50be5be8b8a8d5fb418e423508427ce63acf
Travis Nelson <tjnelson@familysearch.org>
2025-06-08 16:08:50 -0600

    Refactor thank you page and site plan.

---
+++ project/scripts/dates.js
@@ -0,0 +1,5 @@
+// Get the current year for the copyright
+document.getElementById('currentYear').textContent = new Date().getFullYear();
+
+// Get the last modified date of the document
+document.getElementById('lastModified').textContent = new Date(document.lastModified);
