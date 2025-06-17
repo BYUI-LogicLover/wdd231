886e1b301ee35f2a75a8119ad68adf2dff113d67
Travis Nelson <tjnelson@familysearch.org>
2025-06-17 04:59:21 -0600

    Remove files.

--- project/scripts/dates.js
+++
@@ -1,5 +0,0 @@
-// Get the current year for the copyright
-document.getElementById('currentYear').textContent = new Date().getFullYear();
-
-// Get the last modified date of the document
-document.getElementById('lastModified').textContent = new Date(document.lastModified);
